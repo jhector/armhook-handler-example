c06f156a8e5122ed041328d73dae3ba81784e228
Julian Hector <julian.r.hector@gmail.com>
2015-12-04 20:07:13 +0000

    Rewrite code for code running purpose

--- Android.mk
+++ Android.mk
@@ -2,24 +2,12 @@ LOCAL_PATH := $(my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
-	handler.c
+	runner.c
 
 LOCAL_C_INCLUDES += \
 	$(ARMHOOK_ROOT_PATH)/core/helper \
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_MODULE := libhandler
+LOCAL_MODULE := librunner
 include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := \
-	target.c
-
-LOCAL_C_INCLUDES += \
-	$(ARMHOOK_ROOT_PATH)/core/helper \
-
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_MODULE := example_target
-include $(BUILD_EXECUTABLE)
---
+++ runner.c
@@ -0,0 +1,6 @@
+#include <stdint.h>
+
+int32_t code_runner()
+{
+	return 0xdeadbeef;
+}
